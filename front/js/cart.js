a94831af20768a3176448f38da29d7ac30e63ed6
Sana93120 <sana.mktr@icloud.com>
2022-09-18 19:23:56 +0200

    refactored cart.js to include loading event listener

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,18 +2,13 @@
 // TODO pouvoir supprimer un produit affiché (donc suppression aussi dans le local storage)
 // TODO pouvoir modifier la quantité d'un produit affiché (donc modification aussi dans le local storage)
 
-//Initialisation du local storage
-let productsLocalStorage = JSON.parse(localStorage.getItem("produit"));
-console.table(productsLocalStorage);
-const positionEmptyCart = document.querySelector("#cart__items");
-
 // Si le panier est vide
-function getCart(){
+function getCart(productsLocalStorage, positionEmptyCart){
     if (productsLocalStorage === null || productsLocalStorage == 0) {
         const emptyCart = `<p>Votre panier est vide</p>`;
         positionEmptyCart.innerHTML = emptyCart;
-        } else {
-        for (let produit in productsLocalStorage){
+    } else {
+        for (let produit in productsLocalStorage) {
             // Insertion de l'élément "article"
             let productArticle = document.createElement("article");
             document.querySelector("#cart__items").appendChild(productArticle);
@@ -93,143 +88,92 @@ function getCart(){
             productSupprimer.className = "deleteItem";
             productSupprimer.innerHTML = "Supprimer";
         }
-        }
+    }
 }
-getCart();
 
-function getTotals(){
-
+function getTotals(productsLocalStorage) {
     // Récupération du total des quantités
     var elemsQtt = document.getElementsByClassName('itemQuantity');
     var myLength = elemsQtt.length,//la propriété lenght indique le nombre d'élément présent dans le tableau (number)
     totalQtt = 0;
-
     for (var i = 0; i < myLength; ++i) {
         //L'instruction for qui suit débute en déclarant la variable i et en l'initialisant à 0. Elle vérifie que i est inférieur (strictement) à mylenght et exécute ensuite les instructions contenues dans la boucle, ensuite elle incrémente i de 1, ce qui sera fait à chaque passage dans la boucle
         totalQtt += elemsQtt[i].valueAsNumber;//affectation après addition (va calculer la concaténation de ses deux opérateurs)
     }
-
     let productTotalQuantity = document.getElementById('totalQuantity');
     productTotalQuantity.innerHTML = totalQtt;
     console.log(totalQtt);
-
     // Récupération du prix total
     totalPrice = 0;
-
     for (var i = 0; i < myLength; ++i) {
         totalPrice += (elemsQtt[i].valueAsNumber * productsLocalStorage[i].prixProduit);
     }
-
     let productTotalPrice = document.getElementById('totalPrice');
     productTotalPrice.innerHTML = totalPrice;
     console.log(totalPrice);
 }
-getTotals();
 
 // Modification d'une quantité de produit
-function modifyQtt() {
+function modifyQtt(productsLocalStorage) {
     let qttModif = document.querySelectorAll(".itemQuantity");
-
-    for (let k = 0; k < qttModif.length; k++){
+    for (let k = 0; k < qttModif.length; k++) {
         qttModif[k].addEventListener("change" , (event) => {//l'événement change est déclenché pour les éléments input , select, et textarea
             event.preventDefault();//si l'événement n'est pas géré, l'action par défaut ne devrait pas etre éxécuté normalement
-
             //Selection de l'element à modifier en fonction de son id ET sa couleur
             let quantityModif = productsLocalStorage[k].quantiteProduit;
             let qttModifValue = qttModif[k].valueAsNumber;
-            
             const AllreadyExistingProduct  = productsLocalStorage.find((el) => el.qttModifValue !== quantityModif);
-
             AllreadyExistingProduct .quantiteProduit = qttModifValue;
             productsLocalStorage[k].quantiteProduit = AllreadyExistingProduct.quantiteProduit;
-
             localStorage.setItem("produit", JSON.stringify(productsLocalStorage));
-
-            location.reload();
-        
         })
     }
 }
-modifyQtt();
 
 // Suppression d'un produit
-function deleteProduct() {
+function deleteProduct(productsLocalStorage) {
     let btn_supprimer = document.querySelectorAll(".deleteItem");
-
     for (let j = 0; j < btn_supprimer.length; j++){
         btn_supprimer[j].addEventListener("click" , (event) => {//simule un clic de souris sur un élément
             event.preventDefault();//si l'événement n'est pas géré, l'action par défaut ne devrait pas etre éxécuté normalement
-
             //Selection de l'element à supprimer en fonction de son id ET sa couleur
             let idDelete = productsLocalStorage[j].idProduit;
             let colorDelete = productsLocalStorage[j].couleurProduit;
-
             productsLocalStorage = productsLocalStorage.filter( el => el.idProduit !== idDelete || el.couleurProduit !== colorDelete );
-            
             localStorage.setItem("produit", JSON.stringify(productsLocalStorage));
-
             //Alerte produit supprimé et refresh
             alert("Ce produit a bien été supprimé du panier");
-            location.reload();
+            // location.reload();
         })
     }
 }
-deleteProduct();
 
 // un objet regEx est utilisé pour étudier les correspondances d'un texte avec un motif donné
 //les expressions régulières ne sont pas un élément du langage js mais constitue en fait un autre langage en soi
 //Il peut arriver un moment où on a un document rempli de diverses données, éventuellement un qui contient des informations sur une longue liste de personnes, mais on a besoin que des adresses. C'est là que RegEx est utile. on peux utiliser RegEx pour parcourir les caractères du document et localiser une chaîne qui correspond au modèle qu'on spécifie
-
-
 //instauration formulaire avec regex
-
 function getForm() {
     //ajout des regex
     let form = document.querySelector(".cart__order__form");
-
     //création des expressions régulières à voir sur google
     let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
     let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$"); //string expressions
     //d'après ce que j'ai compris: caractère de a à z en minuscule et A à Z en majuscule et chiffre de 0 à 9 + le arobase @ {1 répétition}
-    let addressRegExp = new RegExp("^[0-9]{1,3}(([,. ]?){1}[-a-zA-Zàâäéèêëïîôöùûüç']+)*");
+
+    let addressRegExp = new RegExp("^([0-9]|[-a-zA-Zàâäéèêëïîôöùûüç']|\s)+");
     //question? comment va t-on faire pour une adresse postale américaine par exemple ? 
     //l'expression restera la même ou va t-elle changer ?
-    //regex pour numéro de téléphone francais ^(((\+33\s)|0)[1-9]\s([0-9][0-9]\s){4})$
-
-
-
-    //écoute de la modification du prénom
-    form.firstName.addEventListener('change', function(){
-        validFirstName(this);
-    });
-
-    form.lastName.addEventListener('change', function(){
-        validLastName(this);
-    });
-
-    form.address.addEventListener('change', function(){
-        validAddress(this);
-    });
-
-    form.city.addEventListener('change', function(){
-        validCity(this);
-    });
-
-    form.email.addEventListener('change', function(){
-        validEmail(this);
-    });
+    //regex pour numéro de téléphone francais ^(\+33|0)[1-9]{1}([0-9][0-9]){4}$
 
     //validation du prénom
     const validFirstName = function(inputFirstName) {
         let firstNameErrorMsg = inputFirstName.nextElementSibling;//refers to next elemeent
-
         if (charRegExp.test(inputFirstName.value)) {
             firstNameErrorMsg.innerHTML = '';
         } else {
             firstNameErrorMsg.innerHTML = 'veuillez renseigner ce champ.';
         }
     };
-
     //validation du nom
     const validLastName = function(inputLastName) {
         let lastNameErrorMsg = inputLastName.nextElementSibling;
@@ -240,18 +184,15 @@ function getForm() {
             lastNameErrorMsg.innerHTML = 'veuillez renseigner ce champ.';
         }
     };
-
     //validation de l'adresse 
     const validAddress = function(inputAddress) {
         let addressErrorMsg = inputAddress.nextElementSibling;
-
         if (addressRegExp.test(inputAddress.value)) {
             addressErrorMsg.innerHTML = '';
         } else {
             addressErrorMsg.innerHTML = 'veuillez renseigner votre adresse.';
         }
     };
-
     //valdidation de la ville
     const validCity = function(inputCity) {
         let cityErrorMsg = inputCity.nextElementSibling;
@@ -262,7 +203,6 @@ function getForm() {
             cityErrorMsg.innerHTML = 'veuillez renseigner ce champ.';
         }
     };
-
     //validation du mail
     const validEmail = function(inputEmail) {
         let emailErrorMsg = inputEmail.nextElementSibling;
@@ -273,24 +213,61 @@ function getForm() {
             emailErrorMsg.innerHTML = 'veuillez renseigner votre email.';
         }
     };
+
+    //écoute de la modification du prénom
+    form.firstName.addEventListener('change', function(){
+        validFirstName(this);
+    });
+    form.lastName.addEventListener('change', function(){
+        validLastName(this);
+    });
+    form.address.addEventListener('change', function(){
+        validAddress(this);
+    });
+    form.city.addEventListener('change', function(){
+        validCity(this);
+    });
+    form.email.addEventListener('change', function(){
+        validEmail(this);
+    });
 }
-//getForm();
-
-
-
 
 //Ce qui reste à faire:
 //A-Envoie des informations client au localstorage 
+    // ne pas utiliser location.reload
     //1-créer une fonction que j'appelerais PostForm
     //écouter le panier au clique sur le bouton "commander"
     //construire le array depuis le localstorage
-
-
 function PostForm(){
     const btn_commander = document.getElementById("order");
 
 }
 
 
+
+// TODO separate code that interacts with the DOM with the one that simply defines function
+// TODO also in product.js
+window.addEventListener("DOMContentLoaded", e => {
+
+    //Initialisation du local storage
+    let productsLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+    const positionEmptyCart = document.querySelector("#cart__items");
+
+    // affichage du panier
+    getCart(productsLocalStorage, positionEmptyCart);
+
+    // affichage des totaux
+    getTotals(productsLocalStorage);
+
+    // on enregistre les events listeners de notre page
+    // modification des quantités (event listener)
+    modifyQtt(productsLocalStorage);
+    // suppression des produits (event listener)
+    deleteProduct(productsLocalStorage);
+    // event listener du formulaire
+    getForm();
+
+});
 
 
